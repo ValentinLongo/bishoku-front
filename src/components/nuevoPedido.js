168fb7a7dd49f74006fa93a31cee8f702d57b42d
Valentin <longovalentin@gmail.com>
2024-03-28 11:58:16 -0300

    Nuevo Pedido Parcial

--- src/components/nuevoPedido.js
+++ src/components/nuevoPedido.js
@@ -1,7 +1,128 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table, Button, Space, Input, Form, Typography } from 'antd';
+import axios from 'axios';
 
-const nuevopedido = () => {
-  return <div>Página de Nuevo Pedido</div>;
+const { Text } = Typography;
+
+const NuevoPedido = () => {
+  const [productos, setProductos] = useState([]);
+  const [productosSeleccionados, setProductosSeleccionados] = useState([]);
+  const [nombre, setNombre] = useState('');
+  const [direccion, setDireccion] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://bishoku-back.vercel.app/api/productos');
+        setProductos(response.data);
+      } catch (error) {
+        console.error('Error al obtener productos:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleAgregarProducto = (record) => {
+    setProductosSeleccionados([...productosSeleccionados, record]);
+  };
+
+  const handleEliminarProducto = (record) => {
+    const updatedProductosSeleccionados = productosSeleccionados.filter(
+      (producto) => producto._id !== record._id
+    );
+    setProductosSeleccionados(updatedProductosSeleccionados);
+  };
+
+  const calcularPrecioTotal = () => {
+    return productosSeleccionados.reduce((total, producto) => total + producto.precio, 0);
+  };
+
+  const handleGuardarPedido = () => {
+    console.log('Nombre:', nombre);
+    console.log('Dirección:', direccion);
+    console.log('Productos Seleccionados:', productosSeleccionados);
+    console.log('Precio Total:', calcularPrecioTotal());
+  };
+
+  const columns = [
+    {
+      title: 'Descripción',
+      dataIndex: 'descripcion',
+      key: 'descripcion',
+    },
+    {
+      title: 'Precio',
+      dataIndex: 'precio',
+      key: 'precio',
+      render: (text, record) => `$${text}`,
+    },
+    {
+      title: 'Acción',
+      key: 'action',
+      render: (text, record) => (
+        <Button type="primary" onClick={() => handleAgregarProducto(record)}>
+          Agregar
+        </Button>
+      ),
+    },
+  ];
+
+  const productosSeleccionadosColumns = [
+    {
+      title: 'Descripción',
+      dataIndex: 'descripcion',
+      key: 'descripcion',
+    },
+    {
+      title: 'Precio',
+      dataIndex: 'precio',
+      key: 'precio',
+      render: (text, record) => `$${text}`,
+    },
+    {
+      title: 'Acción',
+      key: 'action',
+      render: (text, record) => (
+        <Button type="primary" danger onClick={() => handleEliminarProducto(record)}>
+          Eliminar
+        </Button>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Carga de Nuevo Pedido</h1>
+      <Form>
+        <Form.Item label="Nombre">
+          <Input value={nombre} onChange={(e) => setNombre(e.target.value)} />
+        </Form.Item>
+        <Form.Item label="Dirección">
+          <Input value={direccion} onChange={(e) => setDireccion(e.target.value)} />
+        </Form.Item>
+      </Form>
+      <Table dataSource={productos} columns={columns} rowKey="_id" />
+      <h2>Productos Seleccionados</h2>
+      <Table
+        dataSource={productosSeleccionados}
+        columns={productosSeleccionadosColumns}
+        rowKey="_id"
+        pagination={false}
+        footer={() => (
+          <>
+            <Text strong>Total:</Text>
+            <Text type="success" strong style={{ marginLeft: '8px' }}>
+              {`$${calcularPrecioTotal()}`}
+            </Text>
+            <Button type="primary" onClick={handleGuardarPedido} style={{ marginLeft: '16px' }}>
+              Guardar Pedido
+            </Button>
+          </>
+        )}
+      />
+    </div>
+  );
 };
 
-export default nuevopedido;
+export default NuevoPedido;
